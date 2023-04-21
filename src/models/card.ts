2034635b11059beb0358083fd960b65262ccb5fa
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2023-04-22 04:24:48 +0500

    start working on second part

--- src/models/card.ts
+++ src/models/card.ts
@@ -1,5 +1,4 @@
-import { ObjectId } from 'mongodb';
-import { Schema, model } from 'mongoose';
+import { ObjectId, Schema, model } from 'mongoose';
 
 type Card = {
   name: String;
@@ -21,21 +20,16 @@ const cardSchema = new Schema<Card>({
     required: [true, 'Ссылка на изображение обязательна для заполнения.'],
   },
   owner: {
-    type: ObjectId,
+    type: Schema.Types.ObjectId,
     required: [true, 'Идентификатор пользователя обязателен для заполнения.'],
   },
   likes: {
-    type: [ObjectId],
+    type: [Schema.Types.ObjectId],
   },
   createdAt: {
     type: Date,
     default: Date.now(),
   },
 });
-/* cardSchema.post('save', { document: true }, function (doc) {
-  doc = doc.toObject();
-  delete doc.__v;
-  console.log(doc);
-}); */
 
 export default model('card', cardSchema);
